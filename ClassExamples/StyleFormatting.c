6b9bd8c07ed1ad3511e37104a64dbab2cc1e2408
Tim-Tscheppe <timothy.tscheppe@marquette.edu>
2023-09-05 11:36:03 -0500

    Clss example

---
+++ ClassExamples/StyleFormatting.c
@@ -0,0 +1,27 @@
+/* 
+* Going to write the examples from class
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    int v = 0; // always initialize variables in embedded
+
+    int b = 0x000B; // 11 in hex
+
+    int vect[10]; // initialize vector of ints that is 10 wide
+
+    return 0;
+}
+
+void BitwiseOperators() {
+
+    // Shift Right >>
+    // Shift Left <<
+    // Bitwise AND &
+    // Bitwise OR |
+    // Exclusive OR ^
+    // Negation ~
+
+}
